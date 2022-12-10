c6a7116579c172cedcfc93b29c726bd2ecc480b0
alexvidalcor <correovidal@outlook.com>
2022-12-10 15:16:49 +0000

    Added verbosity

--- cdk/user_data/install_docker.sh
+++ cdk/user_data/install_docker.sh
@@ -1,6 +1,10 @@
-
-sudo yum update -y
-sudo amazon-linux-extras install docker
-sudo service docker start
-sudo usermod -a -G docker ec2-user
-sudo systemctl enable docker
+'''
+Shebang is added automatically
+You dont need to use sudo inside user data.
+'''
+#! /bin/sh
+yum update -y
+amazon-linux-extras install docker
+service docker start
+usermod -a -G docker ec2-user
+systemctl enable docker
