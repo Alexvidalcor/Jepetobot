bf5f9f01aab71c3f9e89df53b2810184af59d3bf
alexvidalcor <correovidal@outlook.com>
2022-12-10 12:06:55 +0000

    added public_env

--- cdk/app.py
+++ cdk/app.py
@@ -5,11 +5,17 @@ from aws_cdk import (
     Tags
 )
 
+# Python libraries
+from datetime import datetime
+
 # Stack importation
 from cdk_ec2.cdk_ec2_stack import EC2InstanceStack
 
 # Python libraries
 import os
+
+# Custom importation
+import public_env as penv
 
 # Variables from Github Secrets
 awsAccount = os.environ["AWS_ACCOUNT"]
@@ -17,12 +23,18 @@ awsRegion = os.environ["AWS_REGION"]
 awsTagGroupName = os.environ["AWS_TAG_GROUP_NAME"]
 awsTagName = os.environ["AWS_TAG_NAME"]
 
+# Extra variables
+if penv.reusableStack == True:
+    timestamp = datetime.fromtimestamp(1887639468)
+else:
+    timestamp = "managed"
+
 # Set AWS environment
 awsEnv = Environment(account=awsAccount, region=awsRegion)
 
 # Execute stack
 app = App()
-EC2InstanceLayer = EC2InstanceStack(app, "cdk-ec2-deploy", env=awsEnv)
+EC2InstanceLayer = EC2InstanceStack(app, f"cdk-ec2-deploy-{timestamp}", env=awsEnv)
 
 # Add tags
 Tags.of(EC2InstanceLayer).add("Group", awsTagGroupName)
---
+++ cdk/public_env.py
@@ -0,0 +1,3 @@
+# Variables that do not need to be stored as secrets and modify the behavior of the AWS CDK
+
+reusableStack = False
