4e090fc4ccf31983391e96d0f94bcaf3ac06d7fc
alexvidalcor <correovidal@outlook.com>
2022-12-21 22:20:11 +0000

    RMI in codedeploy scripts

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -4,6 +4,7 @@ if [ $( docker ps -a | grep app_cont | wc -l ) -gt 0 ]; then
     echo "app_cont exists"
     docker stop app_cont
     docker rm app_cont
+    docker rmi app_image
 else
         echo "app_cont does not exist"
 fi
