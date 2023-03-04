c1ceff0491db9fac380170f5974b39e2b4078f3d
alexvidalcor <correovidal@outlook.com>
2023-03-04 09:59:17 +0000

    fixed new gpt model issues

--- src/requests.py
+++ src/requests.py
@@ -1,20 +1,20 @@
 # Python libraries
 import openai
 
-#Custom modules
+# Custom modules
 from src.modules.app_support import openaiToken
 
-# Get OpenAI token 
+# Get OpenAI token
 openai.api_key = openaiToken
 
+
 def generate_response(prompt):
-    completions = openai.Completion.create(
+    completions = openai.ChatCompletion.create(
         engine = "gpt-3.5-turbo",
         prompt = prompt,
         max_tokens = 500,
         n = 1,
         stop = None,
-        temperature=0.6,
+        temperature = 0.6,
     )
     return completions["choices"][0]["text"]
-
