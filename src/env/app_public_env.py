f918b95bab59150ba248698da17edf4833233a23
Alex Vidal <58011097+Alexvidalcor@users.noreply.github.com>
2023-11-09 14:15:11 +0100

    ExecLocal False

--- src/env/app_public_env.py
+++ src/env/app_public_env.py
@@ -1,6 +1,6 @@
 # Variables that do not need to be stored as secrets and modify the behavior of the application
 
-execLocal = True
+execLocal = False
 appName = "Jepetobot"
 dbPath = "src/db/MainDb.sqlite"
 logsPath = "src/logs"
