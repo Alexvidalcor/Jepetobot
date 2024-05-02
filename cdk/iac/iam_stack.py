69931e2f639e76c603229cada7b53d28928959b3
alexvidalcor <correovidal@outlook.com>
2024-05-02 16:33:34 +0000

    updated iam names

--- cdk/iac/iam_stack.py
+++ cdk/iac/iam_stack.py
@@ -15,57 +15,57 @@ class IamStack(Stack):
         super().__init__(scope, id, **kwargs)
 
         # Create IAM group
-        iam_group1 = iam.Group(self, appName + "-" + envDeploy + "_Iam-group1",group_name=appName + "-" + envDeploy + "_Iam-group1")
+        iamGroup1 = iam.Group(self, appName + "-" + envDeploy + "_Iam-group1",group_name=appName + "-" + envDeploy + "_Iam-group1")
 
         # Ec2 permissions
-        iam_group1.add_managed_policy(
+        iamGroup1.add_managed_policy(
             iam.ManagedPolicy.from_aws_managed_policy_name(
                 "AmazonEC2FullAccess"
             )
         )
 
         # S3 permissions
-        iam_group1.add_managed_policy(
+        iamGroup1.add_managed_policy(
             iam.ManagedPolicy.from_aws_managed_policy_name(
                 "AmazonS3FullAccess"
             )
         )
 
         # Cloudformation permissions
-        iam_group1.add_managed_policy(
+        iamGroup1.add_managed_policy(
             iam.ManagedPolicy.from_aws_managed_policy_name(
                 "AWSCloudFormationFullAccess"
             )
         )
 
         # Secretmanager permissions
-        iam_group1.add_managed_policy(
+        iamGroup1.add_managed_policy(
             iam.ManagedPolicy.from_aws_managed_policy_name(
                 "SecretsManagerReadWrite"
             )
         )
 
         # Lambda permissions
-        iam_group1.add_managed_policy(
+        iamGroup1.add_managed_policy(
             iam.ManagedPolicy.from_aws_managed_policy_name(
                 "AWSLambda_FullAccess"
             )
         )
 
         # Cloudwatch permissions
-        iam_group1.add_managed_policy(
+        iamGroup1.add_managed_policy(
             iam.ManagedPolicy.from_aws_managed_policy_name(
                 "CloudWatchEventsFullAccess"
             )
         )
 
         # Codedeploy permissions
-        iam_group1.add_managed_policy(
+        iamGroup1.add_managed_policy(
             iam.ManagedPolicy.from_aws_managed_policy_name(
                 "AWSCodeDeployFullAccess"
             )
         )
 
         # Create user in the group created before
-        iam_user1 = iam.User(self, appName + "-" + envDeploy + "_Iam-user1", user_name=appName + "-" + envDeploy + "_Iam-user1")
-        iam_user1.add_to_group(iam_group1)
+        iamUser1 = iam.User(self, appName + "-" + envDeploy + "_Iam-user1", user_name=appName + "-" + envDeploy + "_Iam-user1")
+        iamUser1.add_to_group(iamGroup1)
