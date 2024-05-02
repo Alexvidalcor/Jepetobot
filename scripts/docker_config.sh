db28e22ed8ac68972f8ff853d036c629990b1708
alexvidalcor <correovidal@outlook.com>
2024-05-02 14:44:59 +0000

    new docker args added

--- scripts/docker_config.sh
+++ scripts/docker_config.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd /home/ec2-user/application
-docker build --build-arg awsRegionDocker=$AWS_REGION --build-arg envDeploy=$ENVIRONMENT_DEPLOY -t app_image  .
+docker build --build-arg awsRegionDocker=$AWS_REGION --build-arg envDeploy=$ENVIRONMENT_DEPLOY --build-arg appName=$APP_NAME --build-arg timezone=$TZ -t app_image  .
